3e2854713cafffa121b49deed3ef8693c796df97
holdtomemories <easy_rights.5m@icloud.com>
2026-05-13 01:57:33 +0700

    web bar

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Yearning',
-        short_name: 'Yearning',
-        description: 'leave a part of you somewhere',
-        theme_color: '#0a0a0f',
-        background_color: '#0a0a0f',
-        display: 'standalone',
-        orientation: 'portrait',
-        start_url: '/',
-        icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      }
-    })
   ]
 })
